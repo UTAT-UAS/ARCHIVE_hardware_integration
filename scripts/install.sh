b02ec8577b11f931657593e12edb4ac92c478795
Benjamin Low <benlow999@gmail.com>
2025-04-26 02:55:46 +0000

    gcs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
 bin_dir="$HOME/bin"
-script_name="$HOME/bin/aeac"
 old_dir="$bin_dir/.aeac"
 
 mkdir -p $bin_dir
 mkdir -p $old_dir
 
 clear_bashrc() {
-    # clear
+    sed -i '/#==aeac/d' $HOME/.bashrc
 }
 
 append_bashrc() {
     cp $HOME/.bashrc $HOME/bin/.aeac/.old_bashrc
-    cp $HOME/bin/aeac/.old_bashrc $HOME/bin/aeac/.wifi_bashrc
-    echo "export ROS_MASTER_URI=http://10.5.0.2:11311" >>$HOME/bin/aeac/.wifi_bashrc
-    echo "export ROS_IP=10.5.0.2" >>$HOME/bin/aeac/.wifi_bashrc
+    echo "#==aeac mode tailnet" >>$HOME/.bashrc
+    echo "# export ROS_MASTER_URI=http://10.5.0.2:11311 #==aeac-antenna" >>$HOME/.bashrc
+    echo "# export ROS_IP=10.5.0.2 #==aeac-antenna" >>$HOME/.bashrc
 }
 
 create_bashrc() {
@@ -23,8 +22,42 @@ create_bashrc() {
     append_bashrc
 }
 
+system="drone"
+script_name="$HOME/bin/aeac"
+script=aeac
+
+if [[ $USER == "uas-aeac" ]]; then # guess default
+    system="gcs"
+    script_name="$HOME/bin/gcs"
+    script=gcs
+fi
+
+system_temp="input"
+while true; do
+    echo "choose system - [g]cs, [d]rone"
+    read -p "(default $system): " system_temp
+    if [[ -z $system_temp ]]; then
+        break
+    fi
+    if [[ $system_temp =~ ^g$ ]]; then
+        system="gcs"
+        echo "  gcs"
+        script_name="$HOME/bin/gcs"
+        script=gcs
+        break
+    fi
+    if [[ $system_temp =~ ^d$ ]]; then
+        system="drone"
+        echo "  drone"
+        script_name="$HOME/bin/aeac"
+        script=aeac
+        break
+    fi
+    echo -e $tred"invalid input"$rst
+done
+
 if [[ -f $script_name ]]; then
-    read -p "Do you want to update the script? Ensure .bashrc restored first. (y/n): " response
+    read -p "Do you want to update the script $script_name? (y/n): " response
 
     if [[ "$response" == "y" || "$response" == "Y" ]]; then
         cp ./aeac $HOME/bin
